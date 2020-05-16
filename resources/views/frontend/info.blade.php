b3024dfd56e5d19d9eb40293ad2d14a04a7e3fc9
Fernando Hosting <ferampe@gmail.com>
2020-05-15 22:50:58 -0400

    cambios mifflin

--- resources/views/frontend/info.blade.php
+++ resources/views/frontend/info.blade.php
@@ -3,8 +3,9 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
+	<meta name=viewport content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
 
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 
 	<link href="https://fonts.googleapis.com/css2?family=Spartan:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 
@@ -23,8 +24,8 @@
 				<div class="sidebar-item">
 	                <div class="make-me-sticky">
 						<nav class="menu-container" >
-							<div >
-								<a href="/"><img src="/logo.png" class="img-fluid mb-5" ></a>
+							<div class="logo">
+								<a href="/"><img src="/logo.png" class="mb-4" ></a>
 							</div>
 							
 							@include('frontend.menu')
@@ -136,9 +137,9 @@
 
 	@include('frontend.modal-pago')
 
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
 
 <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery.lazy/1.7.9/jquery.lazy.min.js"></script>
 <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery.lazy/1.7.9/jquery.lazy.plugins.min.js"></script>
--- resources/views/frontend/menu.blade.php
+++ resources/views/frontend/menu.blade.php
@@ -1,4 +1,4 @@
 <div >
 	<a href="{{ route('info') }}" class="menu-item d-block mb-3"><span class="item-info font-weight-bolder">info</span></a>
-	<a href="#" class="menu-item d-block mb-5" data-toggle="modal" data-target="#exampleModal"><span class="item-donar font-weight-bolder">donar ahora</span></a>	
+	<a href="#" class="menu-item d-block mb-4" data-toggle="modal" data-target="#exampleModal"><span class="item-donar font-weight-bolder">donar ahora</span></a>	
 </div>
